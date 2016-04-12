8a266e21f2621225eb94337748b0485d1e90b735
Jeegna Patel <jeegnathebug@users.noreply.github.com>
2016-04-11 20:19:32 -0400

    Update librarytriggers.sql

--- librarytriggers.sql
+++ librarytriggers.sql
@@ -4,14 +4,14 @@ DELIMITER //
 
 CREATE TRIGGER after_book_loan_insert AFTER INSERT ON book_loan FOR EACH ROW
 BEGIN
-    DECLARE genre_number, new_top_reader, current_top_reader INT;
+	DECLARE genre_number, new_top_reader, current_top_reader INT;
 
 	-- get genre of last book inserted
-    SELECT genre INTO genre_number
+	SELECT genre INTO genre_number
 		FROM book_loan
-        INNER JOIN book ON isbn=book
-        ORDER BY loan_id
-        DESC LIMIT 1;
+        	INNER JOIN book ON isbn=book
+        	ORDER BY loan_id
+        	DESC LIMIT 1;
 
 	-- Get new top patron for genre
 	SELECT patron_id INTO new_top_reader
@@ -23,15 +23,15 @@ BEGIN
 		DESC LIMIT 1;
         
 	-- Get current top patron for genre
-    SELECT patron_id INTO current_top_reader
+	SELECT patron_id INTO current_top_reader
 		FROM top_readers
-        WHERE genre_id=genre_number;
+        	WHERE genre_id=genre_number;
 	
 	-- if the top patron has changed
 	IF current_top_reader <> new_top_reader THEN
 		UPDATE top_readers SET patron_id=new_top_reader WHERE genre_id=genre_number;
 	END IF;
-    IF current_top_reader IS NULL THEN
+	IF current_top_reader IS NULL THEN
 		INSERT INTO top_readers VALUES (genre_number, new_top_reader);
-    END IF;
+	END IF;
 END //
